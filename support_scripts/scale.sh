c137f0f279990ecc49531d23f1a776824bfefc44
unknown <206473126@TGC-D1LG8Y1.tfayd.com>
2015-12-18 11:53:52 -0500

    Updated support scripts

--- support_scripts/scale.sh
+++ support_scripts/scale.sh
@@ -3,3 +3,7 @@
 # Simple script to scale the number of Chrome for Firefox instances
 
 docker-compose scale chrome=$1 firefox=$2
+
+
+echo Node ports:
+./get_ports.sh
--- support_scripts/windows_node.sh
+++ support_scripts/windows_node.sh
@@ -3,8 +3,8 @@
 # Simple command to turn this window machine into a node as well and connect it to the selenium hub
 # This script is also execute in run.sh
 
-# Assumes that 'default' is the VM name, edit to to fit yours
-DOCKER_MACHINE_IP=$(docker-machine ip default)
+# Uses the DOCKER_HOST envrionment variable and uses that ip instead, added some cut commands to remove the tcp:// and :[port] crap
+DOCKER_MACHINE_IP=$(echo $DOCKER_HOST | cut -d : -f 2 | cut -d '/' -f 3)
 
 java -jar selenium-server-standalone-2.48.2.jar -role node -hub http://${DOCKER_MACHINE_IP}:4444/grid/register \
 -browser "browserName=internet explorer, maxInstances=5" \
